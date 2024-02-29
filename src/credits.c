314fc4c28f1f885430490b1bf66382196e2e10b5
Glitter <112281668+Glitter-chan@users.noreply.github.com>
2024-02-29 14:37:20 -0800

    Update credits.c
    UwU

--- src/credits.c
+++ src/credits.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-#ifndef BAKERAIN_DEVELOPE_R
-#define BAKERAIN_DEVELOPE_R "kok3shidoll"
-#endif
-#define THE_PLUSH "Ploosh"
+#define SEVERAL "Several People"
 
-#define T void
-#define R print_credits(void) {
-#define O fprintf
-#define U (
-#define B stderr
-#define L ,
-#define E "# == palera1n-c == \n#\n"
-#define M "# Made by: Nick Chan, " THE_PLUSH ", Samara, Mineek, staturnz, " BAKERAIN_DEVELOPE_R " \n#\n"
-#define A "# Thanks to: pythonplayer123, llsc12, Nebula, tihmstar, nikias\n"
-#define K "# (libimobiledevice), checkra1n team (Siguza, axi0mx, littlelailo\n"
-#define I "# et al.), Procursus Team (Hayden Seay, Cameron Katri, Keto et.al)\n\n"
-#define N )
-#define G ;
-#define S }
+void print_credits(void) {
+    fprintf(stderr, "# == era1n-c == \n#\n");
+    fprintf(stderr, "# Made by: Glitter-chan, %s, Walter White, unrealgamer, your mom\n", SEVERAL);
+    fprintf(stderr, "# Thanks to: %s, lrdsnow, circular, katie, James Wallman\n", SEVERAL);
+    fprintf(stderr, "# femra1n team (Glitter-chan, lrdsnow, circular)\n");
+}
 
-T R O U B L E M A K I N G S
+// Call the function to print credits
+print_credits();
